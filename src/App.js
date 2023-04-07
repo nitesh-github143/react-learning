0de93a91bb693df8468d52e90ff7225cb73edf1c
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-04-07 05:57:49 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,26 +2,43 @@ import './App.css';
 import Video from './components/Video';
 
 function App() {
-  let obj = {
-    title: 'React JS tutorial',
-    views: '999K',
-    time: '1 year ago',
-    channel: 'Coder Dost',
-  };
+  let videos = [
+    {
+      id: 1,
+      title: 'React JS tutorial',
+      views: '999K',
+      time: '1 year ago',
+      channel: 'Coder Dost',
+      verified: true
+    }, {
+      id: 2,
+      title: 'Node JS tutorial',
+      views: '100K',
+      time: '1 month ago',
+      channel: 'Coder Dost',
+      verified: false
+    }, {
+      id: 3,
+      title: 'Mongo DB tutorial',
+      views: '1M',
+      time: '1 month ago',
+      channel: 'Coder Dost',
+      verified: true
+    }
+  ];
   return (
     <div className="App">
-      <Video {...obj}></Video>
-      <Video
-        title="Node JS tutorial"
-        views="100K"
-        time="1 month ago"
-      ></Video>
-      <Video
-        title="Mongo DB tutorial"
-        views="1M"
-        time="1 month ago"
-        channel="Coder Dost"
-      ></Video>
+      {
+        videos.map(video => <Video
+          id={video.id}
+          title={video.title}
+          views={video.views}
+          time={video.time}
+          channel={video.channel}
+          verified={video.verified}
+        ></Video>
+        )
+      }
     </div>
   );
 }
